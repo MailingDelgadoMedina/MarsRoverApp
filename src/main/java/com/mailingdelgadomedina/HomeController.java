75408d3c7756645b0c941af58055f7aba6e420be
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-03-31 23:21:51 -0400

    Switch from plain model to modelmap so I can use the CRUD operatio like GET, PUT, POST and DELETE

--- src/main/java/com/mailingdelgadomedina/HomeController.java
+++ src/main/java/com/mailingdelgadomedina/HomeController.java
@@ -1,18 +1,19 @@
 package com.mailingdelgadomedina;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-public String getHomeView(Model model) {
+public String getHomeView(ModelMap model) {
+		//This line is use with the tradition
+		//model.addAttribute("name","Mailing Page");
 		
-		model.addAttribute("name","Mailing Page");
-		
-		
+		model.put("name","Meiling");
 		
 		return "index";
 	}
