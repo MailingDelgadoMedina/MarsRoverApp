8c283fe9c5384e78241d9cb045ddc64636ba79e0
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-03-31 22:55:15 -0400

    Added the HomeController and the view in the templates folder which is the index.html file inside the templates folder

---
+++ src/main/java/com/mailingdelgadomedina/HomeController.java
@@ -0,0 +1,13 @@
+package com.mailingdelgadomedina;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+public String getHomeView() {
+		return "index";
+	}
+}
