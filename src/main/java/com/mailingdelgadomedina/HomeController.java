185c2e4caab721f585298adc56bbfd5722c4f107
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-03-31 23:08:26 -0400

    Added models. Also added thymeleaf to the index.html

--- src/main/java/com/mailingdelgadomedina/HomeController.java
+++ src/main/java/com/mailingdelgadomedina/HomeController.java
@@ -1,13 +1,19 @@
 package com.mailingdelgadomedina;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/")
-public String getHomeView() {
+public String getHomeView(Model model) {
+		
+		model.addAttribute("name","Mailing Page");
+		
+		
+		
 		return "index";
 	}
 }
